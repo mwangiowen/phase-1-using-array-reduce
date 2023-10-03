ad2cc2f925c32452fdc9870415aea4dd26e4efbf
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-03 12:43:24 +0300

    coMpleted

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce(function(sum,ellement){
+    return sum + ellement
+},0)
+console.log(totalBatteries)
 
 // Code your solution here
